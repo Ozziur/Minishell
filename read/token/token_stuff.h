38c76879e99d8752992ca480a2c86cb379a3dc44
Claudio Cantale <ccantale@e4r4p3.42roma.it>
2022-10-03 17:29:12 +0200

    minishell
    boh, cose

--- read/token/token_stuff.h
+++ read/token/token_stuff.h
@@ -6,7 +6,7 @@
 /*   By: mruizzo <mruizzo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 17:55:36 by mruizzo           #+#    #+#             */
-/*   Updated: 2022/10/03 14:26:52 by mruizzo          ###   ########.fr       */
+/*   Updated: 2022/10/03 17:20:36 by ccantale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,10 @@
 
 void	tokenizer_free(void);
 t_token	*next_token(void);
+void	free_token_list(t_token **list);
+void	free_tokens(t_token *token);
+void	free_token_val(t_token *token);
+void	free_env(t_bindings *head);
+
 
 # endif
--- read/token/tokenize.h
+++ read/token/tokenize.h
@@ -6,7 +6,7 @@
 /*   By: mruizzo <mruizzo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 17:55:42 by mruizzo           #+#    #+#             */
-/*   Updated: 2022/10/03 17:10:10 by ccantale         ###   ########.fr       */
+/*   Updated: 2022/10/03 17:28:33 by ccantale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,5 @@
 # include "token_struct.h"
 
 # include "../../utils/utils.h":
-
-void	free_token_list(t_token **list);
-void	free_tokens(t_token *token);
-void	free_token_val(t_token *token);
-void	free_env(t_bindings *head);
 
 #endif
