6dc0f6a1d9d3c88f0ea4204ea719aa2540b2cb90
Claudio Cantale <ccantale@e4r4p3.42roma.it>
2022-10-03 17:07:53 +0200

    minishell\n\nread/token/free_token_list.c

--- read/token/tokenize.h
+++ read/token/tokenize.h
@@ -6,7 +6,7 @@
 /*   By: mruizzo <mruizzo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 17:55:42 by mruizzo           #+#    #+#             */
-/*   Updated: 2022/09/21 18:00:17 by mruizzo          ###   ########.fr       */
+/*   Updated: 2022/10/03 16:00:39 by ccantale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,10 @@
 
 // * standard libraries //
 # include <stdlib.h>
+
+void	free_token_list(t_token **list);
+void	free_tokens(t_token *token);
+void	free_token_val(t_token *token);
+void	free_env(t_bindings *head);
 
 #endif
