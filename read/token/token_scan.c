4b950f0d5fd5766f2adcfc16f3e6463c901d6a93
Michele Ruizzo <mruizzo@e4r4p1.42roma.it>
2022-10-03 17:57:06 +0200

    minishell
    token_scan """"""completa"""""""

--- read/token/token_scan.c
+++ read/token/token_scan.c
@@ -6,7 +6,7 @@
 /*   By: mruizzo <mruizzo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 15:32:32 by mruizzo           #+#    #+#             */
-/*   Updated: 2022/10/03 17:15:27 by mruizzo          ###   ########.fr       */
+/*   Updated: 2022/10/03 17:56:33 by mruizzo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,32 @@
 
 static size_t	scan_head(char *cmd_line, size_t offset, t_token **tok_list_ref)
 {
-	
+	size_t	pre_offset;
+
+	offset = scan_brakets(cmd_line, offset, tok_list_ref);
+	offset = scan_redirs(cmd_line, offset, tok_list_ref);
+	return (offset);
+}
+
+static size_t	scan_body(char *cmd_line, size_t offset, t_token **tok_list_ref)
+{
+	offset = scan_invariants(cmd_line, offset);
+	// if (e_true == is_env_statement(cmd_line, offset))
+	// 	return (scan_env_declaration(cmd_line, offset, tok_list_ref));
+	// else
+	// 	return (scan_simple_command(cmd_line, offset, tok_list_ref));
+	return (offset);//da eliminare
+}
+
+size_t	scan_tail(char *cmd_line, size_t offset, t_token **tok_list_ref)
+{
+	size_t	pre_offset;
+
+	offset = scan_redirs(cmd_line, offset, tok_list_ref);
+	offset = scan_brakets(cmd_line, offset, tok_list_ref);
+	offset = scan_redirs(cmd_line, offset, tok_list_ref);
+	offset = scan_operator(cmd_line, offset, tok_list_ref);
+	return (offset);
 }
 
 void	scan(char *cmd_line, t_token **tok_list_ref)
