f0065b36693ee10e26d89e73e3908e29927ff2d7
Claudio Cantale <ccantale@e4r14p2.42roma.it>
2022-10-22 16:30:31 +0200

    minishell
    fatto env_handler per quello che ci serve. manca solo
    env_list_to_array

--- env/env.h
+++ env/env.h
@@ -6,13 +6,22 @@
 /*   By: ccantale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 18:13:21 by ccantale          #+#    #+#             */
-/*   Updated: 2022/10/21 18:14:09 by ccantale         ###   ########.fr       */
+/*   Updated: 2022/10/22 16:29:17 by ccantale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENV_H
 # define ENV_H
 
-//insert declarations here
+// * user defined types //
+# include "../types.h"
+# include "env_types.h"
+# include "../colors.h"
+
+// * user defined modules //
+# include "../utils/ft_utils.h"
+# include "env_utils/env_utils.h"
+
+void	*env_handler(t_env_handl_opcode opcode, void *argument);
 
 #endif
